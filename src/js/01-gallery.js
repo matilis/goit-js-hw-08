1ef1a1fb8eef13375709e4fd8a063a64849756fd
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-02 20:29:47 +0200

    Task 1 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galleryEl = document.querySelector('.gallery');
+
+for (const image of galleryItems) {
+  const imgEl = `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+ <img class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+  </div>`;
+
+  galleryEl.insertAdjacentHTML('beforeend', imgEl);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
