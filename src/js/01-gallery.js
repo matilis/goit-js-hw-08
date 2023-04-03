60a1af722c2a65f6816eb74944e7d1ca3832d151
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-03 21:56:26 +0200

    Task 1 css update
    Added:
    padding: 0;
    list-style: none;

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,19 +5,13 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector('.gallery');
 
-for (const image of galleryItems) {
-  const imgEl = `<div class="gallery__item">
-  <a class="gallery__link" href=${image.original}>
- <img class="gallery__image"
-      src=${image.preview}
-      data-source=${image.original}
-      alt=${image.description}
-    />
-  </a>
-  </div>`;
-
-  galleryEl.insertAdjacentHTML('beforeend', imgEl);
-}
+const images = galleryItems
+  .map(
+    photo =>
+      `<li class="gallery__item"><a class="gallery__link" href="${photo.original}"><img class="gallery__image" src="${photo.preview}"  alt="${photo.description}"  /></a></li>`
+  )
+  .join('');
+galleryEl.insertAdjacentHTML('beforeend', images);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
