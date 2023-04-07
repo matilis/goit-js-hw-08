dd1e1ba5e9325495daf78c7c73376259a4516aac
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-07 10:06:33 +0200

    Task 2 Finished
    added new way

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -45,3 +45,19 @@ player
         break;
     }
   });
+
+//mega zapis, najbardziej czytelny calosci
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const videoEl = document.getElementById('vimeo-player');
+// const video = new Player(videoEl);
+
+// const saveTime = data =>
+//   localStorage.setItem('videoplayer-current-time', data.seconds);
+// const saveTimeDelay = throttle(saveTime, 1000);
+// const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+// video.setCurrentTime(getCurrentTime).then();
+// video.on('timeupdate', saveTimeDelay);
