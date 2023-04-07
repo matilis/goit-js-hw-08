8b6ede36d40b6d0126e70de25010fc2479e98d45
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-07 18:56:29 +0200

    Task 3 first update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+console.log(iframe);
 const player = new Player(iframe);
 
 player.on(
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+
+class InfoState {
+  state = [];
+  add({ email, message }) {
+    this.state.push({
+      email,
+      message,
+    });
+    this.saveState();
+  }
+  // saveState() {
+  //   throttle(() => {
+  //     localStorage.setItem('feedback-form-state', JSON.stringify(this.state));
+  //   }, 500);
+  // }
+  saveState() {
+    localStorage.setItem('feedback-form-state', JSON.stringify(this.state));
+  }
+  loadState() {
+    const stateLoad = localStorage.getItem('feedback-form-state') || '';
+  }
+}
+const infoState = new InfoState();
+formEl.addEventListener('submit', saveInfo);
+function saveInfo(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+
+  infoState.add({ email, message });
+  event.currentTarget.reset();
+}
